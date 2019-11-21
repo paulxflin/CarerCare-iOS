c7a7f6998ffa90b488e44e5292c314938c0f3af7
Paul Lin <paullam11100@hotmail.com>
2019-11-21 16:46:44 +0000

    Initial Commit

---
+++ AlertApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlertApp
+//
+//  Created by Paul Lam on 21/11/2019.
+//  Copyright © 2019 Team33. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

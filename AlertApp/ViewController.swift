03fbf7082d55d55f5cbbb5a0e1ff7a4cc9a5b1dd
Paul Lin <paullam11100@hotmail.com>
2019-12-09 12:11:50 +0000

    Scroller and message components

--- AlertApp/ViewController.swift
+++ AlertApp/ViewController.swift
@@ -7,13 +7,56 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate, MFMessageComposeViewControllerDelegate  {
+    //MARK: Properties
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    var msg: String?
+        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //Handle the text field's user input through delegate callback
+        messageTextField.delegate = self
     }
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        messageLabel.text = textField.text
+        msg = textField.text! //Unwraps the optional
+    }
+    
+    
+    //MARK: Actions
+    @IBAction func sendMessage(_ sender: UIButton) {
+        if MFMessageComposeViewController.canSendText()
+        {
+            let msgVC = MFMessageComposeViewController()
+            msgVC.body = msg ?? "Example Message Body"
+            msgVC.recipients = ["07460068823"]
+            msgVC.messageComposeDelegate = self
+            self.present(msgVC, animated: true, completion: nil)
+        }
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+
+    
 
 
 }

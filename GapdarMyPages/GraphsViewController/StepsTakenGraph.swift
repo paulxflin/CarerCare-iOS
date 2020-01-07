4e71c44d96f9c2b14a237abfa641be03eabd0fd3
karunya30 <zcabsel@ucl.ac.uk>
2020-01-07 16:07:32 +0000

    pod charts and swapping between pages

---
+++ GapdarMyPages/GraphsViewController/StepsTakenGraph.swift
@@ -0,0 +1,30 @@
+//
+//  StepsTakenGraph.swift
+//  GapdarMyPages
+//
+//  Created by localadmin on 28/12/2019.
+//  Copyright © 2019 localadmin. All rights reserved.
+//
+
+import UIKit
+
+class StepsTakenGraph: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

0318fbdaf1df6c68876ef8d91c1030d28dd5771b
karunya30 <zcabsel@ucl.ac.uk>
2020-02-04 20:42:54 +0000

    2 graphs pages implemented

--- GapdarMyPages/CallsStatisticsViewController.swift
+++ GapdarMyPages/CallsStatisticsViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 import Charts
 import Foundation
 
-class CallsStatisticsViewController: UIViewController, ChartViewDelegate{
+class CallsStatisticsViewController: UIViewController{
 
+   
+    @IBOutlet weak var titleLabel: UILabel!
     
+
+    @IBOutlet weak var graphTitleLabel: UILabel!
     let defaults = UserDefaults.standard
     
     @IBOutlet weak var stepButton: UIButton!
@@ -27,157 +31,24 @@ class CallsStatisticsViewController: UIViewController, ChartViewDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.hidesBackButton = true
-        
-        // MARK: General
-        chartView.delegate = self
-        chartView.drawGridBackgroundEnabled = false
-        chartView.drawBordersEnabled = false
-        chartView.drawBarShadowEnabled      = false
-        chartView.highlightFullBarEnabled   = false
-        chartView.drawOrder                 = [DrawOrder.bar.rawValue,  DrawOrder.line.rawValue]
-        
-        // MARK: xAxis
-        let xAxis                           = chartView.xAxis
-        xAxis.labelPosition                 = .bothSided
-        xAxis.axisMinimum                   = 0.0
-        xAxis.granularity                   = 1.0
-        
-        xAxis.centerAxisLabelsEnabled = true
-        xAxis.setLabelCount( 9, force: true)
-        
-        // MARK: leftAxis
-        let leftAxis                        = chartView.leftAxis
-        leftAxis.drawGridLinesEnabled       = false
-        leftAxis.axisMinimum                = 0.0
-        
-        
-        // MARK: rightAxis
-        let rightAxis                       = chartView.rightAxis
-        rightAxis.drawGridLinesEnabled      = false
-        rightAxis.axisMinimum               = 0.0
-       
-        
-        
-        // MARK: legend
-        let legend                          = chartView.legend
-        legend.wordWrapEnabled              = true
-        legend.horizontalAlignment          = .center
-        legend.verticalAlignment            = .bottom
-        legend.orientation                  = .horizontal
-        legend.drawInside                   = false
-        
-        // MARK: description
-        chartView.chartDescription?.enabled = false
-        
-        setChartData()
+        //self.navigationItem.hidesBackButton = true
+        Graph(chartView: chartView, type: "callsArray").setChartData()
         chartView.backgroundColor = .white
         chartView.layer.cornerRadius = 10.0
         chartView.clipsToBounds = true
     }
+
     
-    func setChartData()
-    {
-        let data = CombinedChartData()
-        data.lineData = generateLineData()
-        data.barData = generateBarData()
-        chartView.xAxis.axisMaximum = data.xMax + 0.25
-        chartView.data = data
+    
+    @IBAction func outdoorButtonPressed(_ sender: Any) {
+        titleLabel.text = "Steps vs Well-being"
+        graphTitleLabel.text = "Outdoor steps and Well-being"
+        Graph(chartView: chartView, type: "stepsArray").setChartData()
     }
-    
-    func getSubArray(_ array : [Int], _ numElems : Int) -> [Int] {
-        var finalArr = [Int](repeating: 0, count: numElems)
-        var i = 0
-        while i < numElems {
-            finalArr[i] = array[i]
-            i += 1
-        }
-        return finalArr
+    @IBAction func callsButtonPressed(_ sender: Any) {
+        titleLabel.text = "Calls vs Well-being"
+        graphTitleLabel.text = "Calls made and Well-being"
+        Graph(chartView: chartView, type: "callsArray").setChartData()
     }
-    
-    
-    func generateLineData() -> LineChartData
-    {
-        // MARK: ChartDataEntry
-        var entries = [ChartDataEntry]()
-        
-        let scoresArray : [Int] = defaults.array(forKey: "scoresArray") as! [Int]
-        let n = 9
-        let wellBeingValues : [Int] = getSubArray(scoresArray, n)
-        
-        //var wellBeingValues: [Int]=[3,5,4,4,9,4,7,6,8]
-        
-        for index in 0..<ITEM_COUNT
-        {
-            entries.append(ChartDataEntry(x: Double(index) + 0.5, y: Double(wellBeingValues[index])))
-        }
-        
-        // MARK: LineChartDataSet
-        let set = LineChartDataSet(values: entries, label: "Well-being score")
-        set.colors = [#colorLiteral(red: 0.941176470588235, green: 0.933333333333333, blue: 0.274509803921569, alpha: 1.0)]
-        set.lineWidth = 2.5
-        set.circleColors = [#colorLiteral(red: 0.941176470588235, green: 0.933333333333333, blue: 0.274509803921569, alpha: 1.0)]
-        set.circleHoleRadius = 2.5
-        set.fillColor = #colorLiteral(red: 0.941176470588235, green: 0.933333333333333, blue: 0.274509803921569, alpha: 1.0)
-        set.drawValuesEnabled = true
-        set.valueFont = NSUIFont.systemFont(ofSize: CGFloat(10.0))
-        set.valueTextColor = #colorLiteral(red: 0.941176470588235, green: 0.933333333333333, blue: 0.274509803921569, alpha: 1.0)
-        set.axisDependency = .left
-        
-        // MARK: LineChartData
-        let data = LineChartData()
-        data.addDataSet(set)
-        
-        
-        return data
-    }
-    
-    func generateBarData() -> BarChartData
-    {
-        // MARK: BarChartDataEntry
-        var entries1 = [BarChartDataEntry]()
-        
-        let callsArray : [Int] = defaults.array(forKey: "callsArray") as! [Int]
-        let n = 9
-        let callsData : [Int] = getSubArray(callsArray, n)
-        print("Calls Data: ")
-        print(callsData)
-        
-        //var callsData: [Int] = [4,10,7,15,20,23,20,13,21]
-        
-        for index in 0..<ITEM_COUNT
-        {
-            entries1.append(BarChartDataEntry(x: Double(index) + 0.5, y: Double(callsData[index])))
-            
-        }
-        
-        // MARK: BarChartDataSet
-        let set1            = BarChartDataSet(values: entries1, label: "Calls made")
-        set1.colors         = [#colorLiteral(red: 0.5224176645, green: 0.5228312612, blue: 0.5224817395, alpha: 1)]
-        set1.valueTextColor = #colorLiteral(red: 0.5208604336, green: 0.5208604336, blue: 0.5208604336, alpha: 1)
-        set1.valueFont      = NSUIFont.systemFont(ofSize: CGFloat(10.0))
-        set1.axisDependency = .right
-        
-        
-        
-        // MARK: BarChartData
-        
-        let barWidth = 0.46
-        
-        // x2 dataset
-        // (0.45 + 0.02) * 2 + 0.06 = 1.00 -> interval per "group"
-        let data = BarChartData(dataSet: set1)
-        data.barWidth = barWidth
-        
-        return data
-    }
-    
-    
-    
-    
-    
-    
-    
-    
 }
 
--- GapdarMyPages/StepsStatisticsViewController.swift
+++ GapdarMyPages/StepsStatisticsViewController.swift
@@ -2,188 +2,48 @@
 //  StepsStatisticsViewController.swift
 //  GapdarMyPages
 //
-//  Created by localadmin on 07/01/2020.
+//  Created by localadmin on 04/02/2020.
 //  Copyright © 2020 localadmin. All rights reserved.
 //
-
 
 import UIKit
 import Charts
 import Foundation
 
-class StepsStatisticsViewController: UIViewController, ChartViewDelegate{
+class StepsStatisticsViewController:UIViewController{
     
-
-    let defaults = UserDefaults.standard
+    @IBOutlet weak var titleLabel: UILabel!
     
-    //@IBOutlet weak var chartView: CombinedChartView!
-
-    @IBOutlet weak var callsButton: UIButton!
+    @IBOutlet weak var graphTitleLabel: UILabel!
     
-    @IBOutlet weak var stepsButton: UIButton!
     @IBOutlet weak var chartView: CombinedChartView!
     
-    //var stepsTakenGraph: UIView!
-    //var wellBeingScoreGraph: UIView!
-    let weeks = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    let ITEM_COUNT = 9
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        self.navigationItem.hidesBackButton = true
-        
-        // MARK: General
-        chartView.delegate = self
-        chartView.drawGridBackgroundEnabled = false
-        chartView.leftAxis.drawAxisLineEnabled = false
-        
-        
-        chartView.drawOrder                 = [DrawOrder.bar.rawValue,  DrawOrder.line.rawValue]
-        
-        // MARK: xAxis
-        let xAxis                           = chartView.xAxis
-        xAxis.labelPosition                 = .bothSided
-        xAxis.axisMinimum                   = 0.0
-       
-        xAxis.centerAxisLabelsEnabled = true
-        xAxis.setLabelCount( 9, force: true)
-        
-        // MARK: leftAxis
-        let leftAxis                        = chartView.leftAxis
-        leftAxis.drawGridLinesEnabled       = false
-        leftAxis.drawAxisLineEnabled = false
-        leftAxis.axisMinimum                = 0.0
-        
-        
-        
-        
-        // MARK: rightAxis
-        let rightAxis                       = chartView.rightAxis
-        rightAxis.drawGridLinesEnabled      = false
-        rightAxis.drawAxisLineEnabled = false
-        rightAxis.axisMinimum               = 0.0
-        
-        
-        
-        
-        // MARK: legend
-        let legend                          = chartView.legend
-        legend.horizontalAlignment          = .center
-        legend.verticalAlignment            = .bottom
-        legend.orientation                  = .horizontal
-        legend.drawInside                   = false
-        
-        // MARK: description
-        chartView.chartDescription?.enabled = false
-        
-        setChartData()
-        chartView.layer.cornerRadius = 10.0
+        //self.navigationItem.hidesBackButton = true
+        Graph(chartView: chartView, type: "stepsArray").setChartData()
         chartView.backgroundColor = .white
+        chartView.layer.cornerRadius = 10.0
         chartView.clipsToBounds = true
     }
     
-    func setChartData()
-    {
-        let data = CombinedChartData()
-        data.lineData = generateLineData()
-        data.barData = generateBarData()
-        chartView.xAxis.axisMaximum = data.xMax + 0.25
-        chartView.data = data
+    
+
+    
+  
+    @IBAction func callsButtonPressed(_ sender: Any) {
+        titleLabel.text = "Calls vs Well-being"
+        graphTitleLabel.text = "Calls made and Well-being"
+        Graph(chartView: chartView, type: "callsArray").setChartData()
     }
     
-    func getSubArray(_ array : [Int], _ numElems : Int) -> [Int] {
-        var finalArr = [Int](repeating: 0, count: numElems)
-        var i = 0
-        while i < numElems {
-            finalArr[i] = array[i]
-            i += 1
-        }
-        return finalArr
+    @IBAction func outdoorButtonPressed(_ sender: Any) {
+        titleLabel.text = "Steps vs Well-being"
+        graphTitleLabel.text = "Outdoor steps and Well-being"
+        Graph(chartView: chartView, type: "stepsArray").setChartData()
     }
-    
-    func generateLineData() -> LineChartData
-    {
-        // MARK: ChartDataEntry
-        var entries = [ChartDataEntry]()
-        
-        let scoresArray : [Int] = defaults.array(forKey: "scoresArray") as! [Int]
-        let n = 9
-        let wellBeingValues : [Int] = getSubArray(scoresArray, n)
-        
-        //var wellBeingValues: [Int]=[3,5,4,4,9,4,7,6,8]
-        
-        for index in 0..<ITEM_COUNT
-        {
-            entries.append(ChartDataEntry(x: Double(index) + 0.5, y: Double(wellBeingValues[index])))
-        }
-        
-        // MARK: LineChartDataSet
-        let set = LineChartDataSet(values: entries, label: "Well-being Score")
-        set.colors = [#colorLiteral(red: 0.941176470588235, green: 0.933333333333333, blue: 0.274509803921569, alpha: 1.0)]
-        set.lineWidth = 2.5
-        set.circleColors = [#colorLiteral(red: 0.941176470588235, green: 0.933333333333333, blue: 0.274509803921569, alpha: 1.0)]
-        set.circleHoleRadius = 2.5
-        set.fillColor = #colorLiteral(red: 0.941176470588235, green: 0.933333333333333, blue: 0.274509803921569, alpha: 1.0)
-        //set.mode = .cubicBezier
-        set.drawValuesEnabled = true
-        set.valueFont = NSUIFont.systemFont(ofSize: CGFloat(10.0))
-        set.valueTextColor = #colorLiteral(red: 0.941176470588235, green: 0.933333333333333, blue: 0.274509803921569, alpha: 1.0)
-        set.axisDependency = .left
-        
-        // MARK: LineChartData
-        let data = LineChartData()
-        data.addDataSet(set)
-        
-        return data
-    }
-    
-    
-    func generateBarData() -> BarChartData
-    {
-        // MARK: BarChartDataEntry
-        var entries1 = [BarChartDataEntry]()
-        
-        let stepsArray : [Int] = defaults.array(forKey: "stepsArray") as! [Int]
-        let n = 9
-        let stepsData : [Int] = getSubArray(stepsArray, n)
-        print("Steps Data: ")
-        print(stepsData)
-        
-        //var stepsData: [Int] = [500,1000,1234,2000,1500,750,2000,1300,2100]
-        
-        for index in 0..<ITEM_COUNT
-        {
-            entries1.append(BarChartDataEntry(x: Double(index) + 0.5, y: Double(stepsData[index])))
-            
-        }
-        
-        // MARK: BarChartDataSet
-        let set1            = BarChartDataSet(values: entries1, label: "Outdoor Steps")
-        set1.colors         = [#colorLiteral(red: 0.1019607843, green: 0.6039215686, blue: 0.662745098, alpha: 1)]
-        set1.valueTextColor = #colorLiteral(red: 0.1019607843, green: 0.6039215686, blue: 0.662745098, alpha: 1)
-        set1.valueFont      = NSUIFont.systemFont(ofSize: CGFloat(10.0))
-        set1.axisDependency = .right
-        
-        
-        
-        // MARK: BarChartData
-       
-        let barWidth = 0.46
-        
-        // x2 dataset
-        // (0.45 + 0.02) * 2 + 0.06 = 1.00 -> interval per "group"
-        let data = BarChartData(dataSet: set1)
-        data.barWidth = barWidth
-       
-        return data
-    }
-    
-   
-    
-    
-    
     
 }
 

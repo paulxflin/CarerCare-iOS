8bde102a9837e31838a60a5b6402a8f877c070a7
Paul Lin <paullam11100@hotmail.com>
2020-02-23 16:57:21 +0000

    added pickers into activity support message

--- GapdarMyPages/ActivitySupportMessage.swift
+++ GapdarMyPages/ActivitySupportMessage.swift
@@ -8,13 +8,90 @@
 
 import UIKit
 
-class ActivitySupportMessage:UIViewController {
+class ActivitySupportMessage:UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+    
+    let defaults = UserDefaults.standard
+    
+    @IBOutlet weak var activityTF: UITextField!
+    @IBOutlet weak var choiceTF: UITextField!
+    
+    @IBOutlet weak var activityPV: UIPickerView!
+    @IBOutlet weak var choicePV: UIPickerView!
+    
+    var activityOptions : [String] = [String]()
+    var choiceOptions : [String] = [String]()
+    
+    var activity : String?
+    var choice : String?
     
     @IBOutlet weak var messageView: UIView!
+    
     override func viewDidLoad() {
         messageView.layer.masksToBounds = true
         messageView.layer.cornerRadius = 10
         
+        activityPV.isHidden = true
+        choicePV.isHidden = true
+        
+        self.activityPV.delegate = self
+        self.choicePV.delegate = self
+        
+        self.activityPV.dataSource = self
+        self.choicePV.dataSource = self
+        
+        activityTF.delegate = self
+        choiceTF.delegate = self
+        
+        activityOptions = defaults.stringArray(forKey: "activityArray") ?? ["Coffee at the Hub", "Shop", "Walk"]
+        choiceOptions = ["Yes", "No"]
+        
+    }
+    
+    // MARK: Actions
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        var numRows = 0
+        if (pickerView == activityPV) {
+            numRows = activityOptions.count
+        } else if (pickerView == choicePV) {
+            numRows = choiceOptions.count
+        }
+        return numRows
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        var option = "";
+        if (pickerView == activityPV) {
+            option = activityOptions[row]
+        } else if (pickerView == choicePV) {
+            option = choiceOptions[row]
+        }
+        return option
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if (pickerView == activityPV) {
+            activity = activityOptions[row]
+            activityTF.text = activity
+            activityPV.isHidden = true
+        } else if (pickerView == choicePV) {
+            choice = choiceOptions[row]
+            choiceTF.text = choice
+            choicePV.isHidden = true
+        }
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        if (textField == activityTF) {
+            activityPV.isHidden = false
+        } else if (textField == choiceTF) {
+            choicePV.isHidden = false
+        }
+        return false
     }
     
     

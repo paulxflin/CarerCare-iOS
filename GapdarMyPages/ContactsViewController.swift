1c6ce0c1e55de5a0c4a5f45a70f07886dbc7c275
Paul Lin <paullam11100@hotmail.com>
2020-02-24 21:10:04 +0000

    updated contact display sizes, added messaging icon

--- GapdarMyPages/ContactsViewController.swift
+++ GapdarMyPages/ContactsViewController.swift
@@ -58,17 +58,20 @@ class ContactsViewController: UIViewController, UITextFieldDelegate, CNContactPi
     }
     
     func addExistingContacts() {
+        let heightOfCanvas = contactSV.frame.height
+        let widthOfCanvas = contactSV.frame.width
+        
         var i = 0
         while i < nameArray.count {
             let name = nameArray[i]
             let phone = phoneArray[i]
             
-            let label = UILabel(frame: CGRect(x:10, y:yContactsValue, width:66, height:33))
+            let label = UILabel(frame: CGRect(x:10, y:yContactsValue, width:66, height: Int(heightOfCanvas/8)))
             label.textAlignment = .left
             label.text = "Name: "
             contactSV.addSubview(label)
             
-            let nameTF = UITextField(frame: CGRect(x:74, y:yContactsValue, width:272, height:33))
+            let nameTF = UITextField(frame: CGRect(x:74, y:yContactsValue, width: (Int(widthOfCanvas - 50)), height:Int(heightOfCanvas/8)))
             nameTF.delegate = self
             nameTF.textAlignment = .center
             nameTF.text = name
@@ -77,14 +80,14 @@ class ContactsViewController: UIViewController, UITextFieldDelegate, CNContactPi
             contactSV.addSubview(nameTF)
             
             nameTextFields.append(nameTF)
-            yContactsValue += 40
+            yContactsValue += Int(heightOfCanvas/8) + 10
             
-            let label2 = UILabel(frame: CGRect(x:10, y:yContactsValue, width:166, height:33))
+            let label2 = UILabel(frame: CGRect(x:10, y:yContactsValue, width:100, height:Int(heightOfCanvas/8)))
             label2.textAlignment = .left
-            label2.text = "Contact Number: "
+            label2.text = "Contact No: "
             contactSV.addSubview(label2)
             
-            let numTF = UITextField(frame: CGRect(x:156, y:yContactsValue, width:150, height:33))
+            let numTF = UITextField(frame: CGRect(x:110, y:yContactsValue, width:Int(widthOfCanvas - 160), height:Int(heightOfCanvas/8)))
             numTF.delegate = self
             numTF.text = phone
             numTF.textAlignment = .center
@@ -95,15 +98,23 @@ class ContactsViewController: UIViewController, UITextFieldDelegate, CNContactPi
             // TODO: Add a button to make calls(God DAMMIT)
             let image = UIImage(named: "icons8-call-32.png") as UIImage?
             let button = UIButton.init(type: .roundedRect)
-            button.frame = CGRect(x:310, y:yContactsValue, width:32, height:32)
+            button.frame = CGRect(x:300, y:yContactsValue, width:32, height:32)
             button.setImage(image, for: .normal)
             button.addTarget(self, action: #selector(callPressed(_ :)), for: .touchUpInside)
             button.tag = i
             contactSV.addSubview(button)
             
+            // Adding a message button
+            let image2 = UIImage(named: "icons8-messaging-50.png") as UIImage?
+            let button2 = UIButton.init(type: .roundedRect)
+            button2.frame = CGRect(x:335, y:yContactsValue, width:32, height:32)
+            button2.setImage(image2, for: .normal)
+            button2.addTarget(self, action: #selector(messagePressed(_ :)), for: .touchUpInside)
+            button2.tag = i
+            contactSV.addSubview(button2)
             
             phoneNumberTextFields.append(numTF)
-            yContactsValue += 40
+            yContactsValue += Int(heightOfCanvas/8) + 10
             
             contactSV.contentSize.height = CGFloat(yContactsValue)
             
@@ -128,6 +139,10 @@ class ContactsViewController: UIViewController, UITextFieldDelegate, CNContactPi
         }
     }
     
+    @objc func messagePressed(_ sender: UIButton) {
+        print("message pressed")
+    }
+    
     
     @IBAction func getNumber(_ sender: Any) {
         let picker = CNContactPickerViewController()
@@ -139,6 +154,11 @@ class ContactsViewController: UIViewController, UITextFieldDelegate, CNContactPi
     
     
     func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
+        
+        // This is where the processing is done after contact is picked
+        // This code is legacy and needs to be changed.
+        
+        // Setting up the variables to collect data
         var familyName = ""
         var phoneNumber = ""
         for data in contact.phoneNumbers{
@@ -147,6 +167,8 @@ class ContactsViewController: UIViewController, UITextFieldDelegate, CNContactPi
             phoneNumber = phoneNo.stringValue
             
         }
+        
+        /*
         let label = UILabel(frame: CGRect(x:44, y:yContactsValue, width:66, height:33))
         //label.center = CGPoint(x:44, y:146)
         label.textAlignment = .center
@@ -176,6 +198,7 @@ class ContactsViewController: UIViewController, UITextFieldDelegate, CNContactPi
         self.view.addSubview(numlabel)
         
         yContactsValue += 40
+        */
     }
     
     func contactPickerDidCancel(_ picker: CNContactPickerViewController) {

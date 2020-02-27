4ea056bff1a49880dd115dd595761276ac6ec450
karunya30 <zcabsel@ucl.ac.uk>
2020-02-27 13:21:54 +0000

    added images

--- GapdarMyPages/ContactsViewController.swift
+++ GapdarMyPages/ContactsViewController.swift
@@ -17,17 +17,29 @@ class ContactsViewController: UIViewController, UITextFieldDelegate, CNContactPi
     var yContactsValue = 10
     var phoneArray : [String] = []
     var nameArray : [String] = []
-    var networkCallsArray : [Int] = []
     
     var phoneNumberTextFields: [UITextField] = []
     var nameTextFields : [UITextField] = []
     
-    var i : Int = 0
-    
     @IBOutlet weak var contactSV: UIScrollView!
     
-    @IBOutlet weak var weblink01: UIImageView!
+    @IBOutlet weak var weblink01: UIButton!
     
+    @IBOutlet weak var weblink02: UIButton!
+    
+    @IBOutlet weak var weblink03: UIButton!
+    
+    @IBOutlet weak var weblink04: UIButton!
+    
+    @IBOutlet weak var weblink05: UIButton!
+    
+    @IBOutlet weak var weblink06: UIButton!
+    
+    @IBOutlet weak var weblink07: UIButton!
+    
+    @IBOutlet weak var weblink08: UIButton!
+    
+    @IBOutlet weak var weblink09: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,32 +47,72 @@ class ContactsViewController: UIViewController, UITextFieldDelegate, CNContactPi
         // Do any additional setup after loading the view, typically from a nib.
         phoneArray = defaults.stringArray(forKey: "phoneArray") ?? []
         nameArray = defaults.stringArray(forKey: "nameArray") ?? []
-        
-        //Create array to track number of calls made per person.
-        if defaults.array(forKey: "networkCallsArray") == nil {
-            networkCallsArray = [Int](repeating: 0, count: nameArray.count)
-            defaults.set(networkCallsArray, forKey: "networkCallsArray")
-            print(networkCallsArray)
-        }
-        networkCallsArray = defaults.array(forKey: "networkCallsArray") as! [Int]
-        
         setupContactSV()
-        setupWebLink01()
     }
     
-    func setupWebLink01()
-    {
-        weblink01.isUserInteractionEnabled = true;
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(webLinkButPressed01))
-        weblink01.addGestureRecognizer(tapGestureRecognizer)
-        weblink01.image = UIImage(named: "web.png")
-    }
-    
-    @objc func webLinkButPressed01()
-    {
-        guard let url = URL(string: "https://google.com") else {return}
+    @IBAction func weblinkButPressed01(_ sender: Any) {
+        guard let url = URL(string: "https://www.torfaen.gov.uk/en/HealthSocialCare/Keeping-Active-and-Getting-Out/Torfaen-Community-Connectors/Torfaen-Community-Connectors.aspx") else {return}
         UIApplication.shared.open(url)
     }
+    
+    @IBAction func weblinkButPressed02(_ sender: Any) {
+        guard let url = URL(string: "https://www.ctsew.org.uk/care-services") else {return}
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func weblinkButPressed03(_ sender: Any) {
+        guard let url = URL(string: "https://www.dewis.wales") else {return}
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func weblinkButPressed04(_ sender: Any) {
+        guard let url = URL(string: "https://www.wales.nhs.uk/sitesplus/866/page/81903") else {return}
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func weblinkButPressed05(_ sender: Any) {
+        guard let url = URL(string: "https://www.ageconnectstorfaen.org.uk/services") else {return}
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func weblinkButPressed06(_ sender: Any) {
+        guard let url = URL(string: "https://www.ffrindimi.co.uk") else {return}
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func weblinkButPressed07(_ sender: Any) {
+        guard let url = URL(string: "whatsapp://") else {return}
+        guard let urlAppStore = URL(string: "https://itunes.apple.com/app/id310633997") else {return}
+        if UIApplication.shared.canOpenURL(url)
+        {
+            UIApplication.shared.open(url)
+        }
+        else
+        {
+            UIApplication.shared.open(urlAppStore)
+        }
+        
+    }
+    
+    @IBAction func weblinkButPressed08(_ sender: Any) {
+        guard let url = URL(string: "https://www.alzheimers.org.uk") else {return}
+        UIApplication.shared.open(url)
+    }
+    
+    @IBAction func weblinkButPressed09(_ sender: Any) {
+        guard let url = URL(string: "headspace://") else {return}
+        guard let urlAppStore = URL(string: "https://itunes.apple.com/app/id493145008") else {return}
+        if UIApplication.shared.canOpenURL(url)
+        {
+            UIApplication.shared.open(url)
+        }
+        else
+        {
+            UIApplication.shared.open(urlAppStore)
+        }
+    }
+    
+    
     
     func setupContactSV() {
         // TODO: Add a for loop to add in the saved contacts.
@@ -70,20 +122,17 @@ class ContactsViewController: UIViewController, UITextFieldDelegate, CNContactPi
     }
     
     func addExistingContacts() {
-        let heightOfCanvas = contactSV.frame.height
-        let widthOfCanvas = contactSV.frame.width
-        
-        //var i = 0
+        var i = 0
         while i < nameArray.count {
             let name = nameArray[i]
             let phone = phoneArray[i]
             
-            let label = UILabel(frame: CGRect(x:10, y:yContactsValue, width:66, height: Int(heightOfCanvas/8)))
+            let label = UILabel(frame: CGRect(x:10, y:yContactsValue, width:66, height:33))
             label.textAlignment = .left
             label.text = "Name: "
             contactSV.addSubview(label)
             
-            let nameTF = UITextField(frame: CGRect(x:74, y:yContactsValue, width: (Int(widthOfCanvas - 50)), height:Int(heightOfCanvas/8)))
+            let nameTF = UITextField(frame: CGRect(x:74, y:yContactsValue, width:272, height:33))
             nameTF.delegate = self
             nameTF.textAlignment = .center
             nameTF.text = name
@@ -92,14 +141,14 @@ class ContactsViewController: UIViewController, UITextFieldDelegate, CNContactPi
             contactSV.addSubview(nameTF)
             
             nameTextFields.append(nameTF)
-            yContactsValue += Int(heightOfCanvas/8) + 10
+            yContactsValue += 40
             
-            let label2 = UILabel(frame: CGRect(x:10, y:yContactsValue, width:100, height:Int(heightOfCanvas/8)))
+            let label2 = UILabel(frame: CGRect(x:10, y:yContactsValue, width:166, height:33))
             label2.textAlignment = .left
-            label2.text = "Contact No: "
+            label2.text = "Contact Number: "
             contactSV.addSubview(label2)
             
-            let numTF = UITextField(frame: CGRect(x:110, y:yContactsValue, width:Int(widthOfCanvas - 160), height:Int(heightOfCanvas/8)))
+            let numTF = UITextField(frame: CGRect(x:156, y:yContactsValue, width:150, height:33))
             numTF.delegate = self
             numTF.text = phone
             numTF.textAlignment = .center
@@ -110,23 +159,15 @@ class ContactsViewController: UIViewController, UITextFieldDelegate, CNContactPi
             // TODO: Add a button to make calls(God DAMMIT)
             let image = UIImage(named: "icons8-call-32.png") as UIImage?
             let button = UIButton.init(type: .roundedRect)
-            button.frame = CGRect(x:300, y:yContactsValue, width:32, height:32)
+            button.frame = CGRect(x:310, y:yContactsValue, width:32, height:32)
             button.setImage(image, for: .normal)
             button.addTarget(self, action: #selector(callPressed(_ :)), for: .touchUpInside)
             button.tag = i
             contactSV.addSubview(button)
             
-            // Adding a message button
-            let image2 = UIImage(named: "icons8-messaging-50.png") as UIImage?
-            let button2 = UIButton.init(type: .roundedRect)
-            button2.frame = CGRect(x:335, y:yContactsValue, width:32, height:32)
-            button2.setImage(image2, for: .normal)
-            button2.addTarget(self, action: #selector(messagePressed(_ :)), for: .touchUpInside)
-            button2.tag = i
-            contactSV.addSubview(button2)
             
             phoneNumberTextFields.append(numTF)
-            yContactsValue += Int(heightOfCanvas/8) + 10
+            yContactsValue += 40
             
             contactSV.contentSize.height = CGFloat(yContactsValue)
             
@@ -140,30 +181,15 @@ class ContactsViewController: UIViewController, UITextFieldDelegate, CNContactPi
         let phone = phoneArray[sender.tag]
         let number = String(phone.filter {!" \n\t\r".contains($0)})
         print(number)
-        
         var totalCalls = defaults.integer(forKey: "totalCalls")
         totalCalls += 1
         defaults.set(totalCalls, forKey: "totalCalls")
         print(defaults.integer(forKey: "totalCalls"))
         
-        networkCallsArray[sender.tag] += 1
-        defaults.set(networkCallsArray, forKey: "networkCallsArray")
-        print("No. Calls per person")
-        print(defaults.array(forKey: "networkCallsArray") as! [Int])
-        
         if let url = URL(string: "tel://\(number)"),
             UIApplication.shared.canOpenURL(url) {
             UIApplication.shared.open(url)
         }
-    }
-    
-    @objc func messagePressed(_ sender: UIButton) {
-        print("message pressed")
-        let messagesSB : UIStoryboard = UIStoryboard(name: "Messages", bundle: nil)
-        let composeVC = messagesSB.instantiateViewController(withIdentifier: "compose")
-        let appDelegate = UIApplication.shared.delegate
-        appDelegate?.window??.rootViewController = composeVC
-        appDelegate?.window??.makeKeyAndVisible()
     }
     
     
@@ -177,11 +203,6 @@ class ContactsViewController: UIViewController, UITextFieldDelegate, CNContactPi
     
     
     func contactPicker(_ picker: CNContactPickerViewController, didSelect contact: CNContact) {
-        
-        // This is where the processing is done after contact is picked
-        // This code is legacy and needs to be changed.
-        
-        // Setting up the variables to collect data
         var familyName = ""
         var phoneNumber = ""
         for data in contact.phoneNumbers{
@@ -190,87 +211,43 @@ class ContactsViewController: UIViewController, UITextFieldDelegate, CNContactPi
             phoneNumber = phoneNo.stringValue
             
         }
-        
-        nameArray.append(familyName)
-        defaults.set(nameArray, forKey: "nameArray")
-        phoneArray.append(phoneNumber)
-        defaults.set(phoneArray, forKey: "phoneArray")
-        
-        networkCallsArray.append(0)
-        defaults.set(networkCallsArray, forKey: "networkCallsArray")
-        
-        
-        let heightOfCanvas = contactSV.frame.height
-        let widthOfCanvas = contactSV.frame.width
-        
-        let name = familyName  //nameArray[i]
-        let phone = phoneNumber  //phoneArray[i]
-        
-        let label = UILabel(frame: CGRect(x:10, y:yContactsValue, width:66, height: Int(heightOfCanvas/8)))
-        label.textAlignment = .left
+        let label = UILabel(frame: CGRect(x:44, y:yContactsValue, width:66, height:33))
+        //label.center = CGPoint(x:44, y:146)
+        label.textAlignment = .center
         label.text = "Name: "
-        contactSV.addSubview(label)
+        self.view.addSubview(label)
         
-        let nameTF = UITextField(frame: CGRect(x:74, y:yContactsValue, width: (Int(widthOfCanvas - 90)), height:Int(heightOfCanvas/8)))
-        nameTF.delegate = self
-        nameTF.textAlignment = .center
-        nameTF.text = name
-        nameTF.backgroundColor = .white
-        nameTF.borderStyle = .roundedRect
-        contactSV.addSubview(nameTF)
+        let namelabel = UITextField(frame: CGRect(x:118, y:yContactsValue, width:272, height:33))
+        //namelabel.center = CGPoint(x:118, y:152)
+        namelabel.textAlignment = .center
+        namelabel.text = familyName
+        namelabel.backgroundColor = .white
+        self.view.addSubview(namelabel)
         
-        nameTextFields.append(nameTF)
-        yContactsValue += Int(heightOfCanvas/8) + 10
+        yContactsValue += 40
         
-        let label2 = UILabel(frame: CGRect(x:10, y:yContactsValue, width:100, height:Int(heightOfCanvas/8)))
-        label2.textAlignment = .left
-        label2.text = "Contact No: "
-        contactSV.addSubview(label2)
+        let label2 = UILabel(frame: CGRect(x:44, y:yContactsValue, width:166, height:33))
+        //label.center = CGPoint(x:44, y:146)
+        label2.textAlignment = .center
+        label2.text = "Contact Number: "
+        self.view.addSubview(label2)
         
-        let numTF = UITextField(frame: CGRect(x:110, y:yContactsValue, width:Int(widthOfCanvas - 200), height:Int(heightOfCanvas/8)))
-        numTF.delegate = self
-        numTF.text = phone
-        numTF.textAlignment = .center
-        numTF.backgroundColor = .white
-        numTF.borderStyle = .roundedRect
-        contactSV.addSubview(numTF)
+        let numlabel = UITextField(frame: CGRect(x:200, y:yContactsValue, width:150, height:33))
+        //namelabel.center = CGPoint(x:118, y:152)
         
-        // TODO: Add a button to make calls(God DAMMIT)
-        let image = UIImage(named: "icons8-call-32.png") as UIImage?
-        let button = UIButton.init(type: .roundedRect)
-        button.frame = CGRect(x:300, y:yContactsValue, width:32, height:32)
-        button.setImage(image, for: .normal)
-        button.addTarget(self, action: #selector(callPressed(_ :)), for: .touchUpInside)
-        button.tag = i
-        contactSV.addSubview(button)
+        numlabel.text = phoneNumber
+        numlabel.backgroundColor = .white
+        self.view.addSubview(numlabel)
         
-        // Adding a message button
-        let image2 = UIImage(named: "icons8-messaging-50.png") as UIImage?
-        let button2 = UIButton.init(type: .roundedRect)
-        button2.frame = CGRect(x:335, y:yContactsValue, width:32, height:32)
-        button2.setImage(image2, for: .normal)
-        button2.addTarget(self, action: #selector(messagePressed(_ :)), for: .touchUpInside)
-        button2.tag = i
-        contactSV.addSubview(button2)
-        
-        phoneNumberTextFields.append(numTF)
-        yContactsValue += Int(heightOfCanvas/8) + 10
-        
-        contactSV.contentSize.height = CGFloat(yContactsValue)
-        
-        i += 1
-        
+        yContactsValue += 40
     }
     
     func contactPickerDidCancel(_ picker: CNContactPickerViewController) {
         print("It cancelled the contact picker view controller when the cancel button is pressed")
     }
-
+    
     @IBAction func resetStart(_ sender: Any) {
         defaults.set(false, forKey: "setup")
-        defaults.set(0, forKey: "totalCalls")
-        defaults.set(nil, forKey: "networkCallsArray")
-        defaults.set(nil, forKey: "networkMessagesArray")
         let activityCountArray = [0, 0, 0]
         defaults.set(activityCountArray, forKey: "activityCountArray")
     }
